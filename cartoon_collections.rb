8d304d7061b06b6b27a425245f5efff21f4a538c
James Lenahan <james.k.lenahan@gmail.com>
2019-12-02 14:48:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,6 @@ def find_the_cheese(array)
   array.find do |cheese|
     cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   end
- 
+
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
