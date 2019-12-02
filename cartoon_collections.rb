25286e1f8febe0e1354d34471124f12acf78c223
James Lenahan <james.k.lenahan@gmail.com>
2019-12-02 14:47:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end
+ 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
